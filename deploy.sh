4842e9cdebcba9325a088f27b4fd6a78cee94e03
apale7 <1092377056@qq.com>
2022-04-02 16:35:56 +0800

    auto deployed

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,12 @@ npm run build
 python3 deploy.py
 SERVER_PATH="apale@apale7.cn:/home/apale/blog/blog"
 scp -r public $SERVER_PATH #拷贝到服务器
-cd public
-git init
 git add .
 git commit -m "auto deployed"
-git push -f git@github.com:Apale7/Apale7.github.io.git master #发布到github pages
-cd ..
+git push -f git@apale7.cn:apale7/rawBlog.git
+# cd public
+# git init
+# git add .
+# git commit -m "auto deployed"
+# git push -f git@github.com:Apale7/Apale7.github.io.git master #发布到github pages
+# cd ..
